b3d8f0ba466b88c7abecb4ba5a8fac3f0d3d7e9b
Aditya Kaushik <114859735+AdityaKaushik2@users.noreply.github.com>
2023-06-19 21:03:49 +0530

    Add files via upload

---
+++ 1_Variables.js
@@ -0,0 +1,20 @@
+const accountId = 4623752;
+
+let accountEmail = "ashdgjkau@gmail.com";
+
+var accountPassword = "12u93u4";
+
+accountCity = "Jaipur";
+
+// accountId = 24232 // Not Allowed
+
+accountEmail = "sdhiefh@gmail.com";
+
+accountPassword = "123456";
+
+accountCity = "Beengaluru";
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+console.log(accountId);
